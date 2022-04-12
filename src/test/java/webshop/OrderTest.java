0052801852e4237d1743f0f20b1ab34e79f3768e
AdamZsoldos <adam.zsoldos@reddwarfgames.com>
2022-04-13 01:10:15 +0200

    Updated OrderTest

--- src/test/java/webshop/OrderTest.java
+++ src/test/java/webshop/OrderTest.java
@@ -10,13 +10,10 @@ class OrderTest {
 
     @Test
     void testCreate() {
-        Order order = new Order(2, new Product(1, "Milk", "Groceries", 300), 3, LocalDateTime.parse("2022-04-01T10:05"));
+        Order order = new Order(2, 1, 3, LocalDateTime.parse("2022-04-01T10:05"));
         assertEquals(2, order.getId());
-        assertEquals(1, order.getProduct().getId());
-        assertEquals("Milk", order.getProduct().getName());
-        assertEquals("Groceries", order.getProduct().getCategory());
-        assertEquals(300, order.getProduct().getPrice());
-        assertEquals(3, order.getAmount());
+        assertEquals(1, order.getProductId());
+        assertEquals(3, order.getProductAmount());
         assertEquals(LocalDateTime.parse("2022-04-01T10:05"), order.getOrderDate());
     }
 }
