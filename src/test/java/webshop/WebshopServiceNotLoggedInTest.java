cdb7125d48a9abcd88ce13f78034ac13187b22bb
AdamZsoldos <adam.zsoldos@reddwarfgames.com>
2022-04-13 01:11:06 +0200

    Added WebshopServiceNotLoggedInTest

---
+++ src/test/java/webshop/WebshopServiceNotLoggedInTest.java
@@ -0,0 +1,35 @@
+package webshop;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WebshopServiceNotLoggedInTest {
+
+    WebshopService webshopService = new WebshopService(null, null, null);
+
+    @Test
+    void testAddProductToCartWhileNotLoggedIn() {
+        Exception e = assertThrows(IllegalStateException.class, () -> webshopService.addProductToCart(1, 10));
+        assertEquals("Must be logged in", e.getMessage());
+    }
+
+    @Test
+    void testRemoveProductFromCartWhileNotLoggedIn() {
+        Exception e = assertThrows(IllegalStateException.class, () -> webshopService.removeProductFromCart(1));
+        assertEquals("Must be logged in", e.getMessage());
+    }
+
+    @Test
+    void testPlaceOrderWhileNotLoggedIn() {
+        Exception e = assertThrows(IllegalStateException.class, () -> webshopService.placeOrder());
+        assertEquals("Must be logged in", e.getMessage());
+    }
+
+    @Test
+    void testListAllOrdersWhileNotLoggedIn() {
+        Exception e = assertThrows(IllegalStateException.class, () -> webshopService.listAllOrders());
+        assertEquals("Must be logged in", e.getMessage());
+    }
+}
